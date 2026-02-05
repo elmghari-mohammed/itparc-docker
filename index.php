f9bf36e1b80c04bbb381a6afc74951a5885c2896
ahmedbouabdallaoui <bouabdallaouiahmed48@gmail.com>
2026-02-05 19:00:40 +0100

    Suppression du mode débogage (error_reporting)

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-// Activer l'affichage des erreurs pour le débogage
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 $currentTime = time();
 $endOfToday20h = strtotime('today 20:00');
