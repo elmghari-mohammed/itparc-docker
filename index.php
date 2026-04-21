d5080def062daadacd5951385c1aa75ac915c726
elmghari mohammed <mohammed.ef6760@gmail.com>
2026-04-21 09:24:21 +0100

    ajout du point d'entrée de l'application

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$currentTime = time();
+$endOfToday20h = strtotime('today 20:00');
+$sessionLifetime = $endOfToday20h - $currentTime;
+
+// Garantir une durée minimale de 1 heure (3600 secondes)
+if ($sessionLifetime < 3600) {
+    $sessionLifetime = 3600;
+}
+
+ini_set('session.gc_maxlifetime', $sessionLifetime);
+session_set_cookie_params($sessionLifetime);
+
+// Inclure la configuration
+require_once(__DIR__ . '/config/config.php');
+
+// Inclure le routeur
+require_once(__DIR__ . '/core/Router.php');
+
+// Démarrer le routage
+Router::route();
+?>
